f12b3d50aab315e48db80ffa0f23a33e87b5572a
klapanik <lapanikkonstantin@gmail.com>
2026-06-21 14:28:57 +0300

    create new model

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
 
-# Create your models here.
 class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
+
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    # Создаёт экземпляр класса Topic тем самым, связывая эти две модели
+    # Также создаёться каскадное удаление
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+        # Просто для того, чтобы множественное число экземпляров джанго читал как 'entries'
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
