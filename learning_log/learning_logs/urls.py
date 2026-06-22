d35a38aecf1c9a8f32264d86f4dee0f862654bb3
klapanik <lapanikkonstantin@gmail.com>
2026-06-22 21:44:12 +0300

    add urls

---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = "learning_logs"  # Чтобы django отличал этот файл urls.py от других
+urlpatterns = [  # список страниц, которые можно запрашивать из приложения learning_logs (например)
+    path("", views.index, name="index") # path('url', функция, имя index чтобы корректно на неё ссылаться)
+]
