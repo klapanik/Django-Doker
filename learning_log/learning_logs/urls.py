31312e472d86ac747c8672ed92baaea8f61364da
klapanik <lapanikkonstantin@gmail.com>
2026-06-24 10:38:36 +0300

    add topics page

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "learning_logs"  # Чтобы django отличал этот файл urls.py от других
 urlpatterns = [  # список страниц, которые можно запрашивать из приложения learning_logs (например)
-    path("", views.index, name="index") # path('url', функция, имя index чтобы корректно на неё ссылаться)
+    path("", views.index, name="index"), # path('url', функция, имя index чтобы корректно на неё ссылаться)
+    path("topics/", views.topics, name="topics"),
 ]
