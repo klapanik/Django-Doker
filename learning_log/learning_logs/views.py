dd8d5d0b8f8991e1d1383ff41edffbe0da410d2c
klapanik <lapanikkonstantin@gmail.com>
2026-06-25 21:20:45 +0300

    create new page for adding entries

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -16,18 +16,38 @@ def topics(request):
 
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topic.html', context)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
+    return render(request, "learning_logs/topic.html", context)
+
 
 def new_topic(request):
-    if request.method != 'POST':
+    if request.method != "POST":
         form = TopicForm()
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save() # записывает данные о форме в базу данных
-            return redirect('learning_logs:topics')
-    
-    context = {'form': form}
-    return render(request, 'learning_logs/new_topic.html', context)
+            form.save()  # записывает данные о форме в базу данных
+            return redirect("learning_logs:topics")
+
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
+
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != "POST":
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            # commit=False указывает пока не сохранять нашу форму но мы записываем все данные с формы в new_entry
+            new_entry.topic = topic  # потом мы создаём новый атрибут
+            new_entry.save()  # и только потом сохраняем новую запись в бд
+            return redirect("learning_logs:topic", topic_id=topic_id)
+            # вторым параметром мы передаём аргумент в функцию представления
+
+    context = {"topic": topic, "form": form}
+    return render(request, "learning_logs/new_entry.html", context)
