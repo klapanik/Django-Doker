829fe1fbc6f5586b59ea1cfc2898defdc218082f
klapanik <lapanikkonstantin@gmail.com>
2026-06-29 14:02:25 +0300

    add url for auth users

--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('learning_logs.urls')),
+    path('users/', include('users.urls')),
     path('', include('learning_logs.urls'))
 ]
