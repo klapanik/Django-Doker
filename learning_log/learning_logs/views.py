b4a77127f50ac99b3c6e9b2fe069228926e831db
klapanik <lapanikkonstantin@gmail.com>
2026-06-30 15:40:40 +0300

    protect user`s topics and entries from editing

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -19,6 +20,10 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
@@ -62,6 +67,9 @@ def new_entry(request, topic_id):
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != "POST":
         form = EntryForm(instance=entry)
