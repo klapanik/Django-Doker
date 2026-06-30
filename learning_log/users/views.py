3335bbbf1479d8bbc864333cc9f6473c5961c17c
klapanik <lapanikkonstantin@gmail.com>
2026-06-30 09:52:39 +0300

    create register page with custom view function

--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method != "POST":
+        form = UserCreationForm() # специальный, встроенный класс
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user) # после успешной регистрации мы осуществляем вход пользователя в аккаунт
+            return redirect("learning_logs:index")
+
+    context = {"form": form}
+    return render(request, "users/register.html", context)
